f6b4c594466a8c8f4de8d925336990e47445256c
Hitesh <gaikwadhitesh03@gmail.com>
2023-07-15 16:23:10 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import SignUpForm from "./Components/SignUpForm.js"
+import "./style.css"
+
+
+const App= ()=>{
+
+    return (
+        <div>
+            <SignUpForm/>
+        </div>
+    )
+}
+
+export default App;
